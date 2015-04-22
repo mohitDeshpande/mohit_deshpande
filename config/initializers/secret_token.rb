6180751f0c75a3fbdac93f68352bd14840da5afc
Mohit Deshpande <mohitdeshpande@icloud.com>
2015-04-22 15:58:31 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MohitDeshpande::Application.config.secret_key_base = 'e63a1c8b0fd19cac00c93598573bbba068e20e0ddfaf76d110b4ac30854d8145fe2257b5ff7ee324ad2eccc4fbc6171a08a6b729ce07341f861534322db2c67d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MohitDeshpande::Application.config.session_store :cookie_store, key: '_mohit_deshpande_session'
